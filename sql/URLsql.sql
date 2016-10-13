443f28d501cc152219702839fe846cd7d04e70b8
hunterhug <gdccmcm14@live.com>
2016-10-13 15:16:41 +0800

    upate

--- sql/URLsql.sql
+++ sql/URLsql.sql
@@ -7,3 +7,7 @@ union
 select * from smart_category where id=101
 union
 select * from smart_category where id=103
+
+
+select id,url,name,level,pid,bigpname,bigpid,ismall from smart_category limit 20000 into outfile 'G:/amzonurl.csv' fields terminated by ',' optionally enclosed by '"' escaped by '"'
+lines terminated by '\r\n';
